007c164f44f968f709957d2a8794bb96ae7d3c39
Iftee1711402 <tawsifhaque97@gmail.com>
2021-09-15 22:34:42 +0600

    add source file

---
+++ movies_list.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+# ---------
+print("\n")
+# ---------
+
+response = requests.get(
+    "https://www.empireonline.com/movies/features/best-movies-2/")
+soup = BeautifulSoup(response.text, "html.parser")
+
+movie_title_tags = soup.select(".jsx-3821216435.listicle-item img")
+# print(len(movie_title_tags))
+# print(movie_title_tags[0].get("alt"))
+
+movies_list = []
+for tag in movie_title_tags:
+    movies_list.append(tag.get("alt"))
+
+# print(movies_list)
+# print(len(movies_list))
+
+for movie in movies_list:
+    with open("movies_list.txt", "a") as file:
+        file.write(f"{movie}\n")
+
+print("done writing to file")
+
+# ---------
+print("\n")
+# ---------
